7107a01e0bd1756214435fcc5a293cfd019d2686
MohamESalem <159151573+MohamESalem@users.noreply.github.com>
2024-03-01 17:38:37 +0200

    added elements to regiester page and testing lines in the main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "loginwindow.h"
-
+#include "register.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoginWindow w;
+    Register r;
+    r.show();
     w.show();
     return a.exec();
 }
