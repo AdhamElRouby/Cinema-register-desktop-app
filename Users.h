83122aa28642146098f98f886aabc5d928980f0a
MohamESalem <159151573+MohamESalem@users.noreply.github.com>
2024-03-01 13:41:13 +0200

    Modified Users.h and finished it, check for edits

--- Users.h
+++ Users.h
@@ -1,4 +1,15 @@
 #ifndef USERS_H
 #define USERS_H
 
+#include <QString>
+
+// These are 3 global arrays of maximam size 100 for: usernames, passwords, and ages
+extern QString usernames[100];
+extern QString passwords[100];
+extern int ages[100];
+//th usersCount int is for current number of users: initally 0
+extern int usersCount;
+
+
+
 #endif // USERS_H
